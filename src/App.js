e512bc0dad6911f1383d5335b1b5d149f9a5f232
StaubRacing <staubracing@gmail.com>
2023-12-01 12:29:04 -0600

    Update Babel configuration and remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Hello } from './components/Hello';
+import Hello from './components/Hello';
+
 
 function App() {
     return (
         <div>
             <h1>This is from the app.js file</h1>
-            {/* More components can be added here */}
-
             <Hello />
-
         </div>
     );
 }
