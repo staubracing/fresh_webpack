072f30c28b100b7cc3b81fbcf3cb4236df721ab9
StaubRacing <staubracing@gmail.com>
2023-12-01 08:44:21 -0600

    intial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello there, React!</h1>
+            {/* More components can be added here */}
+
+        </div>
+    );
+}
+
+export default App;
