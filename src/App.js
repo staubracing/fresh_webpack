da8f981387700ccf9868ce5fbdfda966250a9d90
StaubRacing <staubracing@gmail.com>
2023-12-01 13:02:11 -0600

    adding a restore point for testing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import Hello from './components/Hello';
+// import Hello from './components/Hello';
 
 
 function App() {
     return (
         <div>
-            <h1>This is from the app.js file</h1>
-            <Hello />
+            <h1>This is from the app.js file FRESH_WEBPACK</h1>
+            {/* <Hello /> */}
         </div>
     );
 }
+console.log('App.js is running!');
+console.log(`react version: ${React.version}`);
 
 export default App;
